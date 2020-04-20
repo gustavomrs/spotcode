e92808fd31349970acbcacbfc76e6ba0f90b9862
Gustavo Marques <gustavoufms@gmail.com>
2020-04-19 23:03:43 -0300

    add endpoints for albums, categories, favorites, search and recently heard

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,13 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   has_one_attached :image
+
+  def artists
+    Artist.joins(:albums).where(albums: { id: self.albums.ids }).distinct
+  end
+
+  def songs
+    # Song.joins(:album).where(songs: { id: self.albums.ids }).distinct
+    Song.joins(:album).where(albums: { category_id: self.id }).distinct
+  end
 end
