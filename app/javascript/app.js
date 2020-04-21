0806019d9ef43851e7793ce7df1adf56320ec99c
Gustavo Marques <gustavoufms@gmail.com>
2020-04-21 11:12:31 -0300

    add react and first compoenents

---
+++ app/javascript/app.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import 'react-bulma-components/dist/react-bulma-components.min.css'
+
+const App = () => {
+  return(
+    <Fragment>
+      Hello App
+    </Fragment>
+  )
+}
+
+export default App;
---
+++ app/javascript/screens/albums/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const AlbumsScreen = () => {
+  return (
+    <Fragment>
+      <div>Albums Screen</div>
+    </Fragment>
+  )
+}
+
+export default AlbumsScreen;
