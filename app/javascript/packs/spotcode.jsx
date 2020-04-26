2a8305eb13c0d2cc09d839d0f69ec0f6cb3cbdbd
Gustavo Marques <gustavoufms@gmail.com>
2020-04-26 11:29:32 -0300

    home screen in progress

--- app/javascript/packs/spotcode.jsx
+++ app/javascript/packs/spotcode.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import App from '../app'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   ReactDOM.render(
     <App/>,
     document.getElementById('root'),
--- app/javascript/screens/home/index.js
+++ app/javascript/screens/home/index.js
@@ -1,10 +1,34 @@
 import React, { Fragment } from 'react'
+import SectionWrapper from '../../components/common/section_wrapper'
+import { Columns, Heading } from 'react-bulma-components'
+import styled from 'styled-components'
+
+const MainHeading = styled(Heading)`
+  margin-top: 50px;
+`
+
 
 const HomeScreen = () => {
   return (
-    <Fragment>
-      <div>Home Screen</div>
-    </Fragment>
+    <SectionWrapper>
+      <Columns>
+        <Columns.Column>
+          <MainHeading className='has-text-weight-light has-text-centered has-text-white' size={1}>
+            YOUR APP OF <br/> <span className='has-text-success'>MUSIC</span>
+          </MainHeading>
+        </Columns.Column>
+      </Columns>
+
+      <Columns>
+        <Columns.Column>
+        </Columns.Column>
+      </Columns>
+
+      <Columns>
+        <Columns.Column>
+        </Columns.Column>
+      </Columns>
+    </SectionWrapper>
   )
 }
 
