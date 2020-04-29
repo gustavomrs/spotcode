eae92bab94534f4cecca039d4723874b51384b6d
Gustavo Marques <gustavoufms@gmail.com>
2020-04-29 08:54:46 -0300

    add album components

---
+++ app/javascript/components/album/index.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import { Image, Heading } from 'react-bulma-components'
+import { Link } from 'react-router-dom'
+
+const DivSpaced = styled.div`
+  margin-top: 50px;
+`
+
+const Album = (props) => {
+  return (
+    <Link to={`/albums/${props.id}`}>
+      <Fragment>
+        <Image src={props.cover_url}></Image>
+        <DivSpaced>
+          <Heading size={6} className='has-text-white' >{props.title}</Heading>
+          <Heading size={6} className='has-text-white' subtitle>{props.artist_name}</Heading>
+        </DivSpaced>
+      </Fragment>
+    </Link>
+  )
+}
+
+export default Album;
--- app/javascript/screens/albums/index.js
+++ app/javascript/screens/albums/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
+import NavbarFooter from '../../components/common/navbar_footer'
 
 const AlbumsScreen = () => {
   return (
     <Fragment>
       <div>Albums Screen</div>
+      <NavbarFooter/>
     </Fragment>
   )
 }
