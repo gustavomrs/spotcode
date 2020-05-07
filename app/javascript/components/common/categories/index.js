05d87c1a8ef5d9230e251afae49b9a33a975178c
Gustavo Marques <gustavoufms@gmail.com>
2020-05-06 21:00:39 -0300

    categories list

--- app/javascript/components/common/categories/index.js
+++ app/javascript/components/common/categories/index.js
@@ -1,12 +1,17 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import CategoriesService from '../../../services/categories'
 import { Image, Columns, Section } from 'react-bulma-components'
+import styled from 'styled-components'
+
+const DivSpaced = styled.div`
+  margin-top: 50px;
+`
 
 const Categories = (props) => {
   const [categories, setCategories] = useState([])
 
   async function fetchCategories() {
-    const response = CategoriesService.index()
+    const response = await CategoriesService.index()
     setCategories(response.data['categories'])
   }
 
@@ -14,9 +19,19 @@ const Categories = (props) => {
     fetchCategories()
   }, [])
 
+  const categoriesList = categories.map((category, key) =>
+    <Columns.Column desktop={{size: 3}} mobile={{size: 6}} key={key}>
+      <Image src={category.img_url} onClick={() => props.fetchCategorySearch(category.id)}></Image>
+    </Columns.Column>
+  )
+
   return (
     <Fragment>
-      <div>Categories</div>
+      <DivSpaced>
+        <Columns className='is-mobile'>
+          {categoriesList}
+        </Columns>
+      </DivSpaced>
     </Fragment>
   )
 }
